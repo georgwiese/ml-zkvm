053b9c4fbbc403177c6abf015af03ea264324c84
Georg Wiese <georgwiese@gmail.com>
2023-08-30 15:54:46 +0000

    Full matrix + ReLU example

--- src/ml_zkvm.asm
+++ src/ml_zkvm.asm
@@ -4,8 +4,7 @@ machine Main {
     reg X1[<=];
     reg Y0[<=];
     reg Y1[<=];
-    reg Z0[<=];
-    reg Z1[<=];
+    reg Z[<=];
     reg A0;
     reg A1;
     reg B0;
@@ -34,10 +33,6 @@ machine Main {
         X0IsPositive * (1 - X0IsPositive) = 0;
         // Works for X in [-255, 256]
         X0 + 256 - 1 = X0Byte + X0IsPositive * 256;
-        
-        col witness X1IsPositive;
-        X1IsPositive * (1 - X1IsPositive) = 0;
-        X1 + 256 - 1 = X1Byte + X1IsPositive * 256;
     }
 
     instr jmpz X0, l: label { pc' = X0IsZero * l + (1 - X0IsZero) * (pc + 1) }
@@ -45,27 +40,34 @@ machine Main {
     instr dec_CNT { CNT' = CNT - 1 }
     instr assert_zero X0 { X0IsZero = 1 }
 
-    instr dot X0, X1, Y0, Y1 -> Z0 {
-        Z0 = X0 * Y0 + X1 * Y1
+    instr dot X0, X1, Y0, Y1 -> Z {
+        Z = X0 * Y0 + X1 * Y1
     }
 
-    instr relu X0, X1 -> Z0, Z1 {
-        Z0 = X0IsPositive * X0,
-        Z1 = X1IsPositive * X1
+    instr relu X0 -> Z {
+        Z = X0IsPositive * X0
     }
 
     // Loads a given matrix into [[A0, A1], [B0, B1]] and right-multiplies it with [[1, 2], [3, 4]]
     function main {
 
+        // Loads a given matrix into [[A0, A1], [B0, B1]]
         A0 <=X0= ${ ("input", 0) };
         A1 <=X0= ${ ("input", 1) };
         B0 <=X0= ${ ("input", 2) };
         B1 <=X0= ${ ("input", 3) };
 
+        // Right-multiply with [[1, -2], [3, -4]]
         C0 <== dot(A0, A1, 1, 3);
-        C1 <== dot(A0, A1, 2, 4);
+        C1 <== dot(A0, A1, -2, -4);
         D0 <== dot(B0, B1, 1, 3);
-        D1 <== dot(B0, B1, 2, 4);
+        D1 <== dot(B0, B1, -2, -4);
+
+        // ReLU
+        C0 <== relu(C0);
+        C1 <== relu(C1);
+        D0 <== relu(D0);
+        D1 <== relu(D1);
 
         return;
     }
