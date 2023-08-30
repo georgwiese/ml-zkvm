eef1e839a963668fe45177aa656c3c493bbb283a
Georg Wiese <georgwiese@gmail.com>
2023-08-30 15:55:30 +0000

    Full matrix + ReLU example

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 set -e
 
-powdr pil -o build -f src/ml_zkvm.asm -i 1,0,0,1 --export-csv --field gl
+powdr pil -o build -f src/ml_zkvm.asm -i 1,0,1,0 --export-csv --field gl
 csvtool -u TAB namedcol Row,A0,A1,B0,B1,C0,C1,D0,D1 build/columns.csv | head -n 30
